49d18bdb8e36b06903c90704929c16f8dbc3e624
Sony Theakanath <stheakanath@gmail.com>
2012-08-18 21:01:13 -0700

    Initial Commit

---
+++ BCP Mobile HD/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BCP Mobile HD
+//
+//  Created by Sony Theakanath on 8/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BCP Mobile HD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BCP Mobile HD
+//
+//  Created by Sony Theakanath on 8/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
